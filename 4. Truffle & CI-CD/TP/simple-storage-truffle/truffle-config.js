e9ffabfcc37fc0c3ccc6df5fdac33a5b2ebba4dd
tomawok <tomawok.dev@protonmail.com>
2022-03-25 17:38:38 +0100

    Cours Hardhat

--- 4. Truffle & CI-CD/TP/simple-storage-truffle/truffle-config.js
+++ 4. Truffle & CI-CD/TP/simple-storage-truffle/truffle-config.js
@@ -4,20 +4,25 @@ require('dotenv').config();
 module.exports = {
 
   networks: {
-    development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+    develop: {
+     host: "localhost",
+     port: 7545,
+     network_id: "5777"
     },
-    ropsten:{
-      provider : function() {return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`https://ropsten.infura.io/v3/${process.env.INFURA_ID}`})},
-      network_id:3,
-    },
-    kovan:{
-      provider : function() {return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`https://kovan.infura.io/v3/${process.env.INFURA_ID}`})},
-      network_id:42,
-      from: '0x55467540b54348a4769396e5Ea4fb489022306bd',
-    }
+    // development: {
+    //  host: "127.0.0.1",     // Localhost (default: none)
+    //  port: 8545,            // Standard Ethereum port (default: none)
+    //  network_id: "*",       // Any network (default: none)
+    // },
+    // ropsten:{
+    //   provider : function() {return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`https://ropsten.infura.io/v3/${process.env.INFURA_ID}`})},
+    //   network_id:3,
+    // },
+    // kovan:{
+    //   provider : function() {return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`https://kovan.infura.io/v3/${process.env.INFURA_ID}`})},
+    //   network_id:42,
+    //   from: '0x55467540b54348a4769396e5Ea4fb489022306bd',
+    // }
   },
   
   mocha: {
--- 4. Truffle & CI-CD/hardtest/hardhat.config.js
+++ 4. Truffle & CI-CD/hardtest/hardhat.config.js
@@ -16,6 +16,41 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
-};
+ const HDWalletProvider = require('@truffle/hdwallet-provider');
+ require('dotenv').config();
+ 
+ module.exports = {
+ 
+   networks: {
+     develop: {
+      host: "localhost",
+      port: 7545,
+      network_id: "5777"
+     },
+     // development: {
+     //  host: "127.0.0.1",     // Localhost (default: none)
+     //  port: 8545,            // Standard Ethereum port (default: none)
+     //  network_id: "*",       // Any network (default: none)
+     // },
+     // ropsten:{
+     //   provider : function() {return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`https://ropsten.infura.io/v3/${process.env.INFURA_ID}`})},
+     //   network_id:3,
+     // },
+     // kovan:{
+     //   provider : function() {return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`https://kovan.infura.io/v3/${process.env.INFURA_ID}`})},
+     //   network_id:42,
+     //   from: '0x55467540b54348a4769396e5Ea4fb489022306bd',
+     // }
+   },
+   
+   mocha: {
+     // timeout: 100000
+   },
+ 
+   // Configure your compilers
+   compilers: {
+     solc: {
+       version: "0.8.13",
+     }
+   },
+ };
