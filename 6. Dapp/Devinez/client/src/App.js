5f5fd083b6b79cd08ef73976f1978cdaf57168d1
tomawok <tomawok.dev@protonmail.com>
2022-04-20 23:34:33 +0200

    cours - app Devinez

--- 6. Dapp/Devinez/client/src/App.js
+++ 6. Dapp/Devinez/client/src/App.js
@@ -45,33 +45,78 @@ class App extends Component {
     }
   };
 
+  runGuess = async () => {
+    const { accounts, contract } = this.state;
+    let guess = document.getElementById("guess").value;
+    await contract.methods.playWord(guess).send({ from: accounts[0]} );
+
+    let text;
+    const gagnant = contract.methods.gagnant().call();
+    if(accounts[0] == gagnant) {
+      text = "Bravo, vous avez gagné la partie, attendez que l'admin en lance une nouvelle";
+    } else if(gagnant != 0x0000000000000000000000000000000000000000) {
+      text = "Un autre joueuer a gagné gagné la partie, attendez que l'admin en lance une nouvelle";  
+    } else {
+      text = "Personne n'a encore gagné, tentez votre chance!"
+    }
+
+    this.setState({ text });    
+  }
+
+  runReset = async () => {
+    const { accounts, contract } = this.state;
+    let mot = document.getElementById("mot").value;
+    let indice = document.getElementById("mot").value;
+
+    await contract.methods.reset(mot, indice).send({ from: accounts[0] });
+    
+    let text = "Vous avez reset le jeu et indiqué un nouveau mot";
+    
+    this.setState({ text });
+  }
+
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        {/* <h2>Smart Contract Example</h2>
-        <div>The stored Value is: {this.state.storageValue}</div>
-        <p>
-          Change value :
-        </p>
-        <input type="text" id="valeur" />
-        <button onClick={(this.runSet)}>Set the value you wrote inside the blockchain</button>
-        <br />
-        <p>
-          Addresses that interacted with the contract, and the value they put
-        </p>
-        <table>
-          <tr><td>Address From</td><td>Value Written</td></tr>
-          {this.state.addresses.map((addresse) => (
-            <tr><td>{addresse.returnValues.addr}</td><td>{addresse.returnValues.data}</td></tr>
-          ))}
-        </table> */}
-      </div>
-    );
+    if (this.state.owned) {
+      return (
+        <div className="App">
+          <Adresse addr={this.state.accounts[0]} />
+          <h1>Deviner c'est gagner</h1>
+          <p>Voici l'etat du jeu : </p>
+          <h2>{this.state.text}</h2>
+          <p>Vous pouvez jouer maintenant</p>
+          <p> L'indice est : {this.state.indice}</p>
+          <input type="text" id="guess" />
+          <button onClick={this.runSet}>Essayer</button>
+          <br />
+          <br />
+          <p>C'est toi l'admin gros
+          <br />
+            <input type="text" id="mot" placeholder="mot" />
+            <br />
+            <input type="text" id="indice" placeholder="indice" />
+            <br />
+            <button onClick={this.runReset}>Reset le jeu</button>
+          </p>
+        </div>
+        );
+      } else {
+        return (
+          <div className="App">
+          <Adresse addr={this.state.accounts[0]} />
+          <h1>Deviner c'est gagner</h1>
+          <p>Voici l'etat du jeu : </p>
+          <h2>{this.state.text}</h2>
+          <p>Vous pouvez jouer maintenant</p>
+          <p> L'indice est : {this.state.indice}</p>
+          <input type="text" id="guess" />
+          <button onClick={this.runSet}>Essayer</button>
+          <br />
+        </div>
+      );  
+    }
   }
 }
 
