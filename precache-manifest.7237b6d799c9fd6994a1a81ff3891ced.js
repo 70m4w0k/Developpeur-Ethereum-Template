67518e28610e9392b398ce24b243d2a5712b5a0f
tomawok <tomawok.dev@protonmail.com>
2022-04-20 19:15:57 +0200

    Updates

---
+++ precache-manifest.7237b6d799c9fd6994a1a81ff3891ced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da38ea38318ad0d2db2ff22539bd56d7",
+    "url": "/Developpeur-Ethereum-Template/tree/master/6.%20Dapp/SimpleStorageClass/index.html"
+  },
+  {
+    "revision": "6369179941313ca48e16",
+    "url": "/Developpeur-Ethereum-Template/tree/master/6.%20Dapp/SimpleStorageClass/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "60f14e501ab285c7808e",
+    "url": "/Developpeur-Ethereum-Template/tree/master/6.%20Dapp/SimpleStorageClass/static/js/2.d391a28e.chunk.js"
+  },
+  {
+    "revision": "6369179941313ca48e16",
+    "url": "/Developpeur-Ethereum-Template/tree/master/6.%20Dapp/SimpleStorageClass/static/js/main.9fc73ef4.chunk.js"
+  },
+  {
+    "revision": "07c9a9d02c190a372375",
+    "url": "/Developpeur-Ethereum-Template/tree/master/6.%20Dapp/SimpleStorageClass/static/js/runtime-main.89eadc27.js"
+  }
+]);
