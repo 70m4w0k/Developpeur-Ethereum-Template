925da373dfd6b7bea5b6a9f000cd6d03cc6b4a60
tomawok <tomawok.dev@protonmail.com>
2022-04-25 14:08:51 +0200

    DeFi TP - ERC20 Local

---
+++ 7. Finance Décentralisée/DeFintro/truffle-config.js
@@ -0,0 +1,26 @@
+const path = require("path")
+
+module.exports = {
+
+  networks: {
+    develop: {
+      host: "localhost",
+      port: 7545,
+      network_id: "5777"
+     },
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+  },
+
+  mocha: {
+   },
+
+  compilers: {
+    solc: {
+      version: "0.8.13",
+    }
+  },
+};
